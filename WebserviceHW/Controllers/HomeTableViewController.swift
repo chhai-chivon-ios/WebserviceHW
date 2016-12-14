c341e373db22cb4fb77a66cdf7ef7aedd384ab22
Pisith Ly Khmer <kshrd@kshrds-imac-2.local>
2016-12-14 08:09:15 +0700

    next commit

---
+++ WebserviceHW/Controllers/HomeTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeTableViewController.swift
+//  WebserviceHW
+//
+//  Created by KSHRD on 12/14/16.
+//  Copyright © 2016 KSHRD. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 1
+    }
+}
